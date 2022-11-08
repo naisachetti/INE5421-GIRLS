46006824eaab44e067c088b9c986fb0f83f612a7
bnmfw <bernardoborgessandoval@gmail.com>
2022-11-08 20:34:40 -0300

    versao do automato do mai funcionando

--- Automato.py
+++ Automato.py
@@ -2,48 +2,55 @@ from enum import auto
 
 class Automato:
     def __init__(self, filename: str) -> None:
-        self.estados: dict[Estado] = {}
+        self.estados: list[dict] = []
         with open(filename, "r") as arquivo:
+            
+            # Leitura do cabecalho
             self.n_estados: int = int(arquivo.readline().strip())
-            self.estado_inicial: Estado = Estado(arquivo.readline().strip())
-            self.estados_finais: list[Estado] = [Estado(nome) for nome in arquivo.readline().strip().split(",")]
+            inicial: str = arquivo.readline().strip()
+            self.finais: str = arquivo.readline().strip().split(",")
             self.alfabeto: list[str] = arquivo.readline().strip().split(",")
+            
+            # Criacao do estado morto
+            morto: dict = {"nome": "Morto", "final": False}
+            for simbolo in self.alfabeto:
+                morto[simbolo] = morto
+            self.estados.append(morto)
+            
+            # Criacao de todos os estados e suas transicoes
             for line in arquivo:
-                origin_str, simbol, destiny_str = line.strip().split(",")
+                # Leitura da linha
+                origem, simbolo, destino = line.strip().split(",")
 
                 # Criacao dos estados na tansicao caso eles ainda nao existam
-                if not origin_str in self.estados:
-                    self.estados[origin_str] = Estado(origin_str)
-                if not destiny_str in self.estados:
-                    self.estados[destiny_str] = Estado(destiny_str)
+                def novo_estado(nome: str):
+                    if not nome in [estado["nome"] for estado in self.estados]:
+                        estado = {"nome": nome, "final": nome in self.finais}
+                        for simbolo in self.alfabeto:
+                            estado[simbolo] = morto
+                            self.estados.append(estado)
+                            return estado
+                    return [estado for estado in self.estados if estado["nome"] == nome][0]
+                dict_origem = novo_estado(origem)
+                dict_destino = novo_estado(destino)            
                 
                 # Criacao da transicao
-                self.estados[origin_str].add_trans(simbol, self.estados[destiny_str])
+                dict_origem[simbolo] = dict_destino
 
-    def __repr__(self):
-        saida = f"inicial: {self.estado_inicial.id}\n"
-        saida += "finais: "+" ".join([estado.id for estado in self.estados_finais])+"\n"
-        saida += "simbolos: "+" ".join(self.alfabeto)+"\n"
-        saida += "estados: "+" ".join([estado.id for estado in self.estados.values()])+"\n"
-        saida += "transicoes:\n"
-        for estado in self.estados.values():
-            saida += f"{estado}\n"
-        return saida
+        # Identificacao do estado inicial
+        self.estado_inicial = [estado for estado in self.estados if estado["nome"] == inicial][0]
 
-class Estado:
-    def __init__(self, id: str) -> None:
-        self.id = id
-        self.transicoes = {}
-    
-    def add_trans(self, simbol: str, state) -> None:
-        self.transicoes[simbol] = state
-
-    def __repr__(self) -> str:
-        saida = f"{self.id} ->\t"
-        for simbol, state in self.transicoes.items():
-            saida += f"{simbol}:{state.id}\t"
-        return saida
+    def determine(self, entrada: str) -> bool:
+        estado_atual = self.estado_inicial
+        for simbolo in entrada:
+            estado_atual = estado_atual[simbolo]
+        return estado_atual["final"]
 
 
 automato = Automato("automato_exemplo.txt")
-print(automato)
+assert automato.determine("bb") == True
+assert automato.determine("bbbbbbbbbbbbbb") == True
+assert automato.determine("ba") == False
+assert automato.determine("baaaaabab") == True
+assert automato.determine("aaaaaaaaaaaaaabbbbbbbbbbbbbbbbbaaaaaaa") == True
+assert automato.determine("abb") == False
