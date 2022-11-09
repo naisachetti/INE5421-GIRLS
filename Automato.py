bf2ed72cdb240ab9d42cb3a8725940a6f59a5ca6
bnmfw <bernardoborgessandoval@gmail.com>
2022-11-08 21:51:31 -0300

    metodo de copia do automato implementado

--- Automato.py
+++ Automato.py
@@ -1,5 +1,21 @@
+from enum import auto
+from re import S
+
+
 class Automato:
-    def __init__(self, filename: str) -> None:
+    
+    # Cria o automato
+    def __init__(self, filename: str or None) -> None:
+        
+        # Cria um automato vazio se nao receber arquivo
+        if filename is None:
+            self.estados = None
+            self.n_estados = None
+            self.finais = None
+            self.inicial = None
+            self.alfabeto = None
+            return
+        
         self.estados: list[dict] = []
         with open(filename, "r") as arquivo:
             
@@ -19,7 +35,7 @@ class Automato:
             for line in arquivo:
                 # Leitura da linha
                 origem, simbolo, destinos = line.strip().split(",")
-                destinos = set(destinos.split("-")) # Nao Determinismo
+                destinos = list(destinos.split("-")) # Nao Determinismo
 
                 # Criacao dos estados na tansicao caso eles ainda nao existam
                 def novo_estado(nome: str):
@@ -34,7 +50,7 @@ class Automato:
 
                 # Transicao Deterministica
                 if len(destinos) == 1:
-                    dict_destino = novo_estado(destinos)
+                    dict_destino = novo_estado(destinos[0])
                 
                 # Transicao Nao Deterministica
                 else:
@@ -44,22 +60,42 @@ class Automato:
                 dict_origem[simbolo] = dict_destino
 
         # Identificacao do estado inicial
-        self.estado_inicial = [estado for estado in self.estados if estado["nome"] == inicial][0]
+        self.inicial = [estado for estado in self.estados if estado["nome"] == inicial][0]
 
-    def determine(self, entrada: str) -> bool:
-        estado_atual = self.estado_inicial
+    # Retorna uma copia do automato
+    def copy(self):
+        copia = Automato(None)
+        copia.alfabeto = self.alfabeto.copy()
+        copia.finais = self.finais.copy()
+        copia.n_estados = self.n_estados
+        estados_copia = [estado.copy() for estado in self.estados]
+        # Ajusta as referencias para que se refiram as copias nao ao original
+        for estado_copia in estados_copia:
+            for simbolo in copia.alfabeto:
+                estado_copia[simbolo] = [estado for estado in estados_copia if estado_copia[simbolo]["nome"] == estado["nome"]][0]
+        copia.estados = estados_copia
+        copia.inicial = [estado for estado in estados_copia if estado["nome"] == self.inicial["nome"]][0]
+        return copia
+
+    def reconhece(self, entrada: str) -> bool:
+        estado_atual = self.inicial
         for simbolo in entrada:
             estado_atual = estado_atual[simbolo]
             if isinstance(estado_atual, set):
                 raise RuntimeError ("Transicao nao deterministica")
         return estado_atual["final"]
+    
+    def uniao_com(self, other):
+        pass
+
 
 
 # Verifica se o automato ta reconhecendo as palavras corretamente
 automato = Automato("automato_exemplo.txt")
-assert automato.determine("bb") == True
-assert automato.determine("bbbbbbbbbbbbbb") == True
-assert automato.determine("ba") == False
-assert automato.determine("baaaaabab") == True
-assert automato.determine("aaaaaaaaaaaaaabbbbbbbbbbbbbbbbbaaaaaaa") == True
-assert automato.determine("abb") == False
+
+assert automato.reconhece("bb") == True
+assert automato.reconhece("bbbbbbbbbbbbbb") == True
+assert automato.reconhece("ba") == False
+assert automato.reconhece("baaaaabab") == True
+assert automato.reconhece("aaaaaaaaaaaaaabbbbbbbbbbbbbbbbbaaaaaaa") == True
+assert automato.reconhece("abb") == False
