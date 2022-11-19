f951985e75568c5e7f930933341e757f0088b7ac
naisachetti <naisachetti0@gmail.com>
2022-11-18 22:45:40 -0300

    Cálculo dos nodos anuláveis da AS

--- ArvoreSintatica.py
+++ ArvoreSintatica.py
@@ -19,49 +19,83 @@ class ArvoreSintatica:
         # Cria a Arvore Sintatica para uma regex. regex deve ser recebido como argumento
         self.from_regex(regex)
 
-    # Retorna o regex em notação prefixada da AS
-    # Função utilizada para verificação da integridade da AS
-    def get_arvore(self):
+    # Função utilizada para verificação da integridade da AS.
+    # Retorna o regex em notação prefixada da AS com informações sobre nodos anulaveis, 
+    # seus firstpos, lastpos e/ou followpos.
+    # Pode-se selecionar as informações desejadas mudando os parâmetros de controle para True.
+    def get_arvore(self, anulaveis = False, firstpos = False, lastpos = False, followpos = False):
         nodos = Pilha()
-        filhos_visit_nodos = Pilha()
+        filhos_visit = Pilha()
         arvore = ""
 
         nodos.push(self.raiz)
-        filhos_visit_nodos.push(0)
+        filhos_visit.push(0)
         while (nodos.size() > 0):
             if isinstance(nodos.top(), Fecho):
-                if filhos_visit_nodos.top() == 0:
+                if filhos_visit.top() == 0:
                     arvore += '(' + nodos.top().get_tipo()
-                    num_visit = filhos_visit_nodos.pop()
-                    filhos_visit_nodos.push(num_visit + 1)
+                    arvore += self.infos_nodo(nodos.top(), anulaveis, firstpos, lastpos, followpos)
+                    num_visit = filhos_visit.pop()
+                    filhos_visit.push(num_visit + 1)
+                    filhos_visit.push(0)
                     nodos.push(nodos.top().get_filho())
-                    filhos_visit_nodos.push(0)
                 else:
                     arvore += ')'
                     nodos.pop()
-                    filhos_visit_nodos.pop()
+                    filhos_visit.pop()
             elif isinstance(nodos.top(), Concat) or isinstance(nodos.top(), Ou):
-                if filhos_visit_nodos.top() == 0:
+                if filhos_visit.top() == 0:
                     arvore += '(' + nodos.top().get_tipo()
-                    num_visit = filhos_visit_nodos.pop()
-                    filhos_visit_nodos.push(num_visit + 1)
+                    arvore += self.infos_nodo(nodos.top(), anulaveis, firstpos, lastpos, followpos)
+                    num_visit = filhos_visit.pop()
+                    filhos_visit.push(num_visit + 1)
+                    filhos_visit.push(0)
                     nodos.push(nodos.top().get_filho_esq())
-                    filhos_visit_nodos.push(0)
-                elif filhos_visit_nodos.top() == 1:
-                    num_visit = filhos_visit_nodos.pop()
-                    filhos_visit_nodos.push(num_visit + 1)
+                elif filhos_visit.top() == 1:
+                    num_visit = filhos_visit.pop()
+                    filhos_visit.push(num_visit + 1)
                     nodos.push(nodos.top().get_filho_dir())
-                    filhos_visit_nodos.push(0)
+                    filhos_visit.push(0)
                 else:
                     arvore += ')'
                     nodos.pop()
-                    filhos_visit_nodos.pop()
+                    filhos_visit.pop()
             elif isinstance(nodos.top(), Simbolo):
                 arvore += self.simbolos[nodos.top().get_id()]
+                arvore += self.infos_nodo(nodos.top(), anulaveis, firstpos, lastpos, followpos)
                 nodos.pop()
-                filhos_visit_nodos.pop()
+                filhos_visit.pop()
 
         return arvore
+
+    # Retorna uma string com informações sobre nodos anulaveis, seus firstpos, lastpos e/ou followpos.
+    # Pode-se selecionar as informações desejadas mudando os parâmetros de controle para True.
+    def infos_nodo(self, nodo, anulaveis = False, firstpos = False, lastpos = False, followpos = False):
+        infos = ""
+        if anulaveis:
+            if nodo.is_anulavel():
+                infos += "{n}"
+        
+        if firstpos:
+            infos += '{'
+            for id in nodo.get_firstpos():
+                infos += (str)(id)
+            infos += '}'
+
+        if lastpos:
+            infos += '{'
+            for id in nodo.get_lastpos():
+                infos += (str)(id)
+            infos += '}'
+
+        if followpos:
+            if isinstance(nodo, Fecho) or isinstance(nodo, Concat):
+                infos += '{'
+                for id in nodo.get_followpos():
+                    infos += (str)(id)
+                infos += '}'
+
+        return infos
 
     # Cria uma AS de teste para (.(.(.(.(*(+ab))a)b)b)#)
     def criar_teste(self):
@@ -92,6 +126,7 @@ class ArvoreSintatica:
         self.add_simbolo('#', operadores.top())
         operadores.pop()
 
+    # Cria uma AS a partir de uma regex
     def from_regex(self, regex: str):
         regex_processar = '(.'+regex+'#)'
         operadores = Pilha()
@@ -119,6 +154,7 @@ class ArvoreSintatica:
                 # print("Add simbolo")
                 self.add_simbolo(caracter, operadores.top())
 
+    # Adiciona um operador como filho de um determinado nodo da AS
     def add_operador(self, tipo: TipoOp, nodo_pai = None):
         if tipo == TipoOp.FECHO:
             op = Fecho()
@@ -136,36 +172,89 @@ class ArvoreSintatica:
 
         return op
 
+    # Adiciona um simbolo como filho de um determinado nodo da AS
     def add_simbolo(self, caracter: chr, nodo_pai):
-        simbolo = Simbolo(self.cont_id)
+        if (caracter == '&'):
+            simbolo = Simbolo(self.cont_id, anulavel=True)
+        else:
+            simbolo = Simbolo(self.cont_id)
         self.simbolos[self.cont_id] = caracter
         self.cont_id += 1
         nodo_pai.add_filho(simbolo)
 
+    # Faz o calculo para definir nodos anulaveis, firstpos, lastpos e followpos da AS
+    def calcula_infos_nodos(self):
+        nodos = Pilha()
+        filhos_visit = Pilha()
+
+        nodos.push(self.raiz)
+        filhos_visit.push(0)
+        while (nodos.size() > 0):
+            if isinstance(nodos.top(), Fecho):
+                if (filhos_visit.top() == 0):
+                    num_visit = filhos_visit.pop()
+                    filhos_visit.push(num_visit + 1)
+                    nodos.push(nodos.top().get_filho())
+                    filhos_visit.push(0)
+                else:
+                    nodos.top().calcula_firstpos()
+                    nodos.top().calcula_lastpos()
+                    nodos.top().calcula_followpos()
+                    nodos.pop()
+                    filhos_visit.pop()
+            elif isinstance(nodos.top(), Concat) or isinstance(nodos.top(), Ou):
+                if (filhos_visit.top() == 0):
+                    num_visit = filhos_visit.pop()
+                    filhos_visit.push(num_visit + 1)
+                    nodos.push(nodos.top().get_filho_esq())
+                    filhos_visit.push(0)
+                elif (filhos_visit.top() == 1):
+                    num_visit = filhos_visit.pop()
+                    filhos_visit.push(num_visit + 1)
+                    nodos.push(nodos.top().get_filho_dir())
+                    filhos_visit.push(0)
+                else:
+                    nodos.top().calcula_anulavel()
+                    nodos.top().calcula_firstpos()
+                    nodos.top().calcula_lastpos()
+                    if isinstance(nodos.top(), Concat):
+                        nodos.top().calcula_followpos()
+                    nodos.pop()
+                    filhos_visit.pop()
+            elif isinstance(nodos.top(), Simbolo):
+                nodos.pop()
+                filhos_visit.pop()
+
 class Nodo:
 
     # Cria nodo da AS
-    def __init__(self):
-        self.firstpos = []
-        self.lastpos = []
+    def __init__(self, anulavel = False, firstpos = [], lastpos = []):
+        self.anulavel = anulavel
+        self.firstpos = firstpos
+        self.lastpos = lastpos
 
-    def calcula_firstpos(self):
-        pass
+    def is_anulavel(self):
+        return self.anulavel
 
-    def calcula_lastpos(self):
-        pass
+    def get_firstpos(self):
+        return self.firstpos
+    
+    def get_lastpos(self):
+        return self.lastpos
 
 class Fecho(Nodo):
 
     # Cria nodo do tipo Fecho (*) da AS
     def __init__(self):
-        super().__init__()
+        super().__init__(anulavel=True)
         self.filho = None
-        self.anulavel = False
         self.followpos = []
 
     def get_tipo(self):
         return '*'
+
+    def get_followpos(self):
+        return self.followpos
 
     def add_filho(self, novo_filho):
         if (self.filho is None):
@@ -175,8 +264,11 @@ class Fecho(Nodo):
     
     def get_filho(self):
         return self.filho
+        
+    def calcula_firstpos(self):
+        pass
 
-    def calcula_anulavel(self):
+    def calcula_lastpos(self):
         pass
 
     def calcula_followpos(self):
@@ -186,14 +278,16 @@ class Concat(Nodo):
 
     # Cria nodo do tipo Concat (.) da AS
     def __init__(self):
-        super().__init__()
+        super().__init__(anulavel=False)
         self.filho_esq = None
         self.filho_dir = None
-        self.anulavel = False
         self.followpos = []
 
     def get_tipo(self):
         return '.'
+
+    def get_followpos(self):
+        return self.followpos
 
     def add_filho(self, novo_filho):
         if (self.filho_esq is None):
@@ -210,6 +304,15 @@ class Concat(Nodo):
         return self.filho_dir
 
     def calcula_anulavel(self):
+        if self.filho_esq.is_anulavel() and self.filho_dir.is_anulavel():
+            self.anulavel = True
+        else:
+            self.anulavel = False
+
+    def calcula_firstpos(self):
+        pass
+
+    def calcula_lastpos(self):
         pass
 
     def calcula_followpos(self):
@@ -219,10 +322,9 @@ class Ou(Nodo):
 
     # Cria nodo do tipo Ou (+) da AS
     def __init__(self):
-        super().__init__()
+        super().__init__(anulavel=False)
         self.filho_esq = None
         self.filho_dir = None
-        self.anulavel = False
 
     def get_tipo(self):
         return '+'
@@ -242,6 +344,15 @@ class Ou(Nodo):
         return self.filho_dir
 
     def calcula_anulavel(self):
+        if self.filho_esq.is_anulavel() or self.filho_dir.is_anulavel():
+            self.anulavel = True
+        else:
+            self.anulavel = False
+
+    def calcula_firstpos(self):
+        pass
+
+    def calcula_lastpos(self):
         pass
 
 class Simbolo(Nodo):
@@ -250,8 +361,10 @@ class Simbolo(Nodo):
     #
     # O simbolo (caracter) representado por cada nodo fica guardado 
     # em um dicionario na classe ArvoreSintatica, para busca rápida.
-    def __init__(self, id: int):
-        super().__init__()
+    def __init__(self, id: int, anulavel = False):
+        firstpos = [] if anulavel else [id]
+        lastpos = [] if anulavel else [id]
+        super().__init__(anulavel, firstpos, lastpos)
         self.id = id
 
     def get_id(self):
--- tests_ArvoreSintatica.py
+++ tests_ArvoreSintatica.py
@@ -2,6 +2,7 @@ import unittest
 from ArvoreSintatica import *
 
 class VerificarArvoreSintatica(unittest.TestCase):
+    # Testa se AS esta sendo montada corretamente
     def test_get_arvore(self):
         # Arrange
         regex = "(.(.(.(*(+ a b)) a) b) b)"
@@ -13,6 +14,7 @@ class VerificarArvoreSintatica(unittest.TestCase):
         # Assert
         self.assertEqual("(.(.(.(.(*(+ab))a)b)b)#)", arvore)
 
+    # Testa se AS esta sendo montada corretamente
     def test2_get_arvore(self):
         # Arrange
         regex = "(+(.(*(+ab)) (. a b)) a)"
@@ -23,3 +25,29 @@ class VerificarArvoreSintatica(unittest.TestCase):
 
         # Assert
         self.assertEqual("(.(+(.(*(+ab))(.ab))a)#)", arvore)
+
+    # Testa se AS esta sendo montada corretamente
+    def test3_get_arvore(self):
+        # Arrange
+        regex = "(+(.(*(+ab)) (. a b)) &)"
+        a_sint = ArvoreSintatica(regex)
+
+        # Act
+        arvore = a_sint.get_arvore()
+
+        # Assert
+        self.assertEqual("(.(+(.(*(+ab))(.ab))&)#)", arvore)
+
+    # Testa se calculo dos anulaveis esta correto
+    def test4_get_arvore(self):
+        # Arrange
+        regex = "(+(.(*(+ab)) (. a b)) &)"
+        a_sint = ArvoreSintatica(regex)
+
+        # Act
+        arvore = a_sint.calcula_infos_nodos()
+        arvore = a_sint.get_arvore(anulaveis=True)
+
+        # Assert
+        self.assertEqual("(.(+{n}(.(*{n}(+ab))(.ab))&{n})#)", arvore)
+        print(arvore)
