60e5bda936b5a2f327488e885009593afe1830ae
João Mai <joaomai2001@gmail.com>
2023-07-07 10:55:24 -0300

    ajuste no break e makefile

--- GIRLS.py
+++ GIRLS.py
@@ -3,13 +3,15 @@ import sys
 from Gramatica import Gramatica
 from Parser import AnalisadorSintatico
 from Lexer import AnalisadorLexico
+from Semantico import AnalisadorSemantico
 
 class GIRLS:
-    def __init__(self, folder, nome_programa=""):
+    def __init__(self, folder="compiladores", nome_programa=""):
         self.lexer = AnalisadorLexico(folder, nome_programa)
         self.parser = AnalisadorSintatico(folder, self.lexer, validar=False, precompiled=True)
         if self.parser.parse(True):
             print("\nSintaxe e léxico correto do arquivo program")
+            self.semantico = AnalisadorSemantico()
         else:
             print("\nProblema na sintaxe")
 
@@ -20,4 +22,4 @@ if __name__ == '__main__':
         if len(sys.argv) >= 3:
             girls = GIRLS(sys.argv[1], sys.argv[2])
         else:
-            girls = GIRLS(sys.argv[1])
+            girls = GIRLS(nome_programa=sys.argv[1])
